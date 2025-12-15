d32254215a1609ef7d0b6c1311640e78ae167e18
Mohammed Atesam <atesam07@gmail.com>
2025-12-15 07:17:47 +0530

    react-query setup

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://dlbhpcrahsbuugvlnlkt.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsYmhwY3JhaHNidXVndmxubGt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2Mjc2NjQsImV4cCI6MjA4MTIwMzY2NH0.n3_JebVhEU4zvjAtXuo27BDwsVWnwEgJ7K6Ri2Nf5AA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
