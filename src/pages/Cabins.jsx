6e6230e08fbbaea4848066042fc3489e49427c06
Mohammed Atesam <atesam07@gmail.com>
2025-12-21 02:20:55 +0530

    reactQuery Complete

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
+  const [showForm, setShowForm] = useState(false);
 
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      {/* <img src="https://dlbhpcrahsbuugvlnlkt.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" /> */}
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add a cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
   );
 }
 
